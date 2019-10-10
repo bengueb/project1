e07ca7c278a2ba12d266ecc5ff7656a987578742
mkkiyoi <mkkiyoi@gmail.com>
2019-10-09 19:39:25 -0700

    Added google-maps-api.js.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,18 +1,3 @@
 'use strict';
 
-function findBreweriesByNeiborhood(longitude, latitude) {
-    let queryURL = 'https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=' + longitude + ',' + latitude + '&radius=3000&keyword=breweries&key=' + config.placesKey;
-
-    $.ajax({
-        url: queryURL,
-        method: 'GET'
-    }).then(function(response) {
-        let breweries = [];
-        response.results.forEach(function(brewery) {
-            breweries.push(brewery.name);
-        });
-        console.log(breweries);
-    })
-}
-
-findBreweriesByNeiborhood(47.6792172,-122.3860312);
+getBreweries(47.6792172,-122.3860312);
