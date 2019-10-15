293f8e70327c4e47bb7d2fdadd73e4af7f6de188
JC Lenz <jc@miserablepop.com>
2019-10-14 20:59:28 -0700

    Updated id for input on brew search

--- assets/javascript/brewApi.js
+++ assets/javascript/brewApi.js
@@ -46,7 +46,7 @@ var baseUrl = 'https://sandbox-api.brewerydb.com/v2/';
         
 
         newBrewSection.attr('id','brewery-result');
-        $('#resultsArea').append(newBrewSection);
+        $('#resultsArea').appendgit (newBrewSection);
     
     });
 
